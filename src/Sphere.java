e2f1c88b0ba428f2cbe2d336e146dcb993e8de8d
ben-stott <118287374+ben-stott@users.noreply.github.com>
2023-02-27 12:47:39 +0000

    code improvements

--- src/Sphere.java
+++ src/Sphere.java
@@ -10,6 +10,36 @@ public class Sphere {
         colour = c;
     }
 
+    private double Discriminant(double a, double b, double c) {
+        return b*b-4*a*c;
+    }
+
+    private double Quadratic(double a,double b,double c, Boolean close) {
+        double disc = Discriminant(a,b,c);
+        if (close) return (-b-Math.sqrt(disc))/(2*a);
+        else return (-b+Math.sqrt(disc))/(2*a);
+    }
+
+    public boolean checkIntersection(Vector origin, Vector direction) {
+        Vector v = origin.sub(centPos);
+        double a = direction.dot(direction);
+        double b = 2*v.dot(direction);
+        double c = v.dot(v) - radius*radius;
+        double disc = Discriminant(a,b,c);
+        return disc >= 0;
+    }
+
+    public Double findIntersection(Vector origin, Vector direction) {
+        Vector v = origin.sub(centPos);
+        double a = direction.dot(direction);
+        double b = 2*v.dot(direction);
+        double c = v.dot(v) - radius*radius;
+
+        double t = Quadratic(a,b,c,true); // ray sphere intersection, how far along
+        if (t<0) t = Quadratic(a,b,c,false);
+        return t;
+    }
+
     public Vector getCentPos() {
         return centPos;
     }
